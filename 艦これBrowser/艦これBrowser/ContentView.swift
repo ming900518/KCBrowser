77dca048e010e9e8696def73920286e3517c5995
Ming Chang <ming900518@gmail.com>
2020-05-10 22:14:54 +0800

    Upload local files
    Nothing modified, just a blank App.

---
+++ 艦これBrowser/艦これBrowser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  艦これBrowser
+//
+//  Created by Ming Chang on 5/10/20.
+//  Copyright © 2020 Ming Chang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
